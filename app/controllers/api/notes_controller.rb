4eafb248e4e516e88dc9a0f699b5f940c24bef20
Lauren Dove <elledoveinc@gmail.com>
2020-09-02 09:46:03 -0400

    added privte notes_params and notes has create method. both index and create rrenfer using JSON

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -1,5 +1,27 @@
 class Api::NotesController < ApplicationController
     def index
-        
+
+        notes = Note.all
+        render json: notes
+
     end
+    def create
+        note = Note.new(notes_params)
+        if note.save
+            render json: note
+        else
+            render json:{errors: note.errors.full_message}
+    end
+
+
+
+
+private
+
+def notes_params
+
+    params.require(:note).permit(:title,:description,:speaker, :link_url, :topic_id)
+end
+
+
 end
