a46be65571e9686f3394ddd413e037e320d78b7b
Lauren Dove <elledoveinc@gmail.com>
2020-09-02 07:30:55 -0400

    created notes model and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Note.create(title: "Discernment-Don't leave home without it", description:"Love and hate has a thin line. Love will bind you to the people you love and care for. Hate will bind you to the people you hate and have disdain for. A person you hate you will “bring them with you” home and talk about them and that’s all you can focus on. Someone you love you want to share pictures of them with people and talk about how great they are...etc.
+* Discernment is necessary to KNOW what to do at the RIGHT TIME!", speaker: "Bishop Dale C. Bronner", link_url: "https://www.youtube.com/watch?v=OhlmlCM-JLg", topic_id:)
