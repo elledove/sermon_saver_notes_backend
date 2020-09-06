976bf5b41b3e4b2b7286ed289abd7f6c48fce586
Lauren Dove <elledoveinc@gmail.com>
2020-09-05 22:29:45 -0400

    added update method to notes controller and to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
-    resources :notes, only: [:index, :create]
+    resources :notes, only: [:index, :create,:update]
     resources :topics, only:[:index]
   end
 end
